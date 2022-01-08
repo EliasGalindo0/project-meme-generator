c01408bcd70f1f55825641ffee8b6aa7ae5843d8
Elias <eliasgalindo@gmail.com>
2022-01-08 17:04:59 -0300

    Requisito 6 feito - Estiliza as bordas por meio de botoes

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-const textInput = document.getElementById('text-input');
-const textOut = document.getElementById('meme-text');
-const image = document.getElementById('meme-image');
+const textInput = document.querySelector('#text-input');
+const textOut = document.querySelector('#meme-text');
+const image = document.querySelector('#meme-image');
 const imageInput = document.querySelector('#meme-insert');
+const buttonFire = document.querySelector('#fire');
+const buttonWater = document.querySelector('#water');
+const buttonEarth = document.querySelector('#earth');
+const memeImage = document.querySelector('#meme-image-container');
 
 function showText() {
   const text = textInput.value;
@@ -18,5 +22,23 @@ function imageDisplay() {
   reader.readAsDataURL(this.files[0]);
 }
 
+function borderFire() {
+  memeImage.style.border = ' ';
+  memeImage.style.border = '3px dashed red';
+}
+
+function borderWater() {
+  memeImage.style.border = ' ';
+  memeImage.style.border = '5px double blue';
+}
+
+function borderEarth() {
+  memeImage.style.border = ' ';
+  memeImage.style.border = '6px groove green';
+}
+
 textInput.addEventListener('input', showText);
 imageInput.addEventListener('change', imageDisplay);
+buttonFire.addEventListener('click', borderFire);
+buttonWater.addEventListener('click', borderWater);
+buttonEarth.addEventListener('click', borderEarth);

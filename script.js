54e85bb96b578c91f406f8dae0d8fa39deb575c1
Elias <eliasgalindo@gmail.com>
2022-01-09 19:46:00 -0300

    Requisito 7 feito - adiciona imagens prontas

--- script.js
+++ script.js
@@ -6,22 +6,16 @@ const buttonFire = document.querySelector('#fire');
 const buttonWater = document.querySelector('#water');
 const buttonEarth = document.querySelector('#earth');
 const memeImage = document.querySelector('#meme-image-container');
+const meme1 = document.querySelector('#meme-1');
+const meme2 = document.querySelector('#meme-2');
+const meme3 = document.querySelector('#meme-3');
+const meme4 = document.querySelector('#meme-4');
 
 function showText() {
   const text = textInput.value;
   textOut.innerHTML = text;
   return textOut;
 }
-// Código consultado no site https://medium.com/@iamcodefoxx/how-to-upload-and-preview-an-image-with-javascript-749b92711b91
-function imageDisplay() {
-  const reader = new FileReader();
-  reader.addEventListener('load', () => {
-    const uploadedImage = reader.result;
-    image.style.backgroundImage = `url(${uploadedImage})`;
-  });
-  reader.readAsDataURL(this.files[0]);
-}
-
 function borderFire() {
   memeImage.style.border = ' ';
   memeImage.style.border = '3px dashed red';
@@ -37,8 +31,22 @@ function borderEarth() {
   memeImage.style.border = '6px groove green';
 }
 
+function getImageFooter(e) {
+  image.src = e.target.src;
+}
+
 textInput.addEventListener('input', showText);
-imageInput.addEventListener('change', imageDisplay);
+// imageInput.addEventListener('change', imageDisplay);
 buttonFire.addEventListener('click', borderFire);
 buttonWater.addEventListener('click', borderWater);
 buttonEarth.addEventListener('click', borderEarth);
+meme1.addEventListener('click', getImageFooter);
+meme2.addEventListener('click', getImageFooter);
+meme3.addEventListener('click', getImageFooter);
+meme4.addEventListener('click', getImageFooter);
+// Código pesquisado em vídeos no youtube
+imageInput.addEventListener('change', (e) => {
+  image.innerHTML = '';
+  const uploadedImage = e.target.files[0];
+  image.src = URL.createObjectURL(uploadedImage);
+});
